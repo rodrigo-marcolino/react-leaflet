1236937a60b3727f05a02a09a2ca2351403995f6
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2021-12-23 19:31:15 +1300

    dd marker example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
 import './App.css'
 import '../node_modules/leaflet/dist/leaflet.css'
 
 function App() {
-  console.log(process.env.REACT_APP_MAPBOX_API_KEY)
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    })
+  })
   return (
     <MapContainer center={[-37.62998749601141, 176.17162023111158]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url={`https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USERID}/${process.env.REACT_APP_MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_API_KEY}`}
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[-37.62961546215643, 176.1717440597863]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>

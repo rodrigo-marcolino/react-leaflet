6eed507fad8ed668dc002c5f0b3bd5f8c13e3c5a
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2021-12-23 16:43:50 +1300

    add env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css'
+import '../node_modules/leaflet/dist/leaflet.css'
 
 function App() {
-  // const position = [51.505, -0.09]
+  console.log(process.env.REACT_APP_MAPBOX_API_KEY)
   return (
-
+    <MapContainer center={[-37.62998749601141, 176.17162023111158]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={`https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USERID}/${process.env.REACT_APP_MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_API_KEY}`}
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 

19b28044a50e313a5f54c99680ebd05b20a1d018
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2021-12-23 15:44:48 +1300

    clean-up create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './App.css'
 
 function App() {
+  // const position = [51.505, -0.09]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
